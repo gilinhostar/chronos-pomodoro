12d5147be029408dd322badf046bbfbed15df090
gilinhostar <leonardo.gil.t.s@hotmail.com>
2026-05-26 15:48:25 +0100

    css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 //PascalCase
+import { Heading } from "./components/Heading";
+
+import "./styles/global.css";
+import "./styles/theme.css";
 
 export function App() {
   return (
     <>
-      <h1>Hello, world!</h1>;<h1>Olá, Mundo!</h1>
+      <Heading />
+      <h1>Hello, world!</h1>
+      <h1>Olá, Mundo!</h1>
+      <p>Welcome to your new React + TypeScript app.</p>
     </>
   );
 }
