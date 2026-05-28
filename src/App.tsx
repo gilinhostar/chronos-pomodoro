ae4442d95eee09afb236bf83bf223dcdaf5f5dbb
gilinhostar <leonardo.gil.t.s@hotmail.com>
2026-05-28 17:38:13 +0100

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
 import { Heading } from "./components/Heading";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
@@ -22,6 +23,31 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput
+              labelText="task"
+              id="meuInput"
+              type="text"
+              placeholder="Digite algo"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>lorem ipsum dolor sit amet</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
 
       <Container>
